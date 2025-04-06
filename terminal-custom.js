7c7906bac8e6eddf8e1e1c0082cbf6d5b4d65d0e
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2025-04-06 13:26:42 +0530

    Update terminal-custom.js

--- terminal-custom.js
+++ terminal-custom.js
@@ -6,3 +6,10 @@ figelt -c "your-name"
 htop
 
 sudo nano ~/.zshssr => add few lines alias
+
+https://www.youtube.com/watch?v=oFJsZOFnADc => kali in 5 mins
+
+htop => sudo apt install htop
+htop
+htop is a command-line, interactive process viewer and system monitor for Linux, similar to top, but with a more user-friendly and visually appealing interface, allowing you to easily see and manage running processes. 
+
